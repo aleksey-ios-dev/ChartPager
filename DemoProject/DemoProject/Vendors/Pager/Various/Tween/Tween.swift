a3be541c1ab6cf96f364c48b044ff3a216396c1f
Aleksey Chernish <achernish85@gmail.com>
2015-09-21 13:48:14 +0700

    minor adjustments on code

--- DemoProject/DemoProject/Vendors/Pager/Various/Tween/Tween.swift
+++ DemoProject/DemoProject/Vendors/Pager/Various/Tween/Tween.swift
@@ -28,11 +28,11 @@ class Tween {
 
         layer = {
             let layer = TweenLayer()
-            object.layer.addSublayer(layer)
             layer.from = from
             layer.to = to
             layer.tweenDuration = duration
             layer.animationDelegate = self
+            object.layer.addSublayer(layer)
 
             return layer
         }()
@@ -63,6 +63,57 @@ extension Tween: TweenLayerDelegate {
 
     func tweenLayerDidStopAnimation(layer: TweenLayer) {
         layer.removeFromSuperlayer()
+    }
+}
+
+protocol TweenLayerDelegate: class {
+    func tweenLayer(layer: TweenLayer, didSetAnimatableProperty to: CGFloat) -> Void
+    func tweenLayerDidStopAnimation(layer: TweenLayer) -> Void
+}
+
+class TweenLayer: CALayer {
+    @NSManaged private var animatableProperty: CGFloat
+
+    var animationDelegate: TweenLayerDelegate?
+
+    var from: CGFloat = 0
+    var to: CGFloat = 0
+    var tweenDuration: NSTimeInterval = 0
+    var timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
+    var delay: NSTimeInterval = 0
+
+    override class func needsDisplayForKey(event: String) -> Bool {
+        return event == "animatableProperty" ? true : super.needsDisplayForKey(event)
+    }
+
+    override func actionForKey(event: String) -> CAAction? {
+        if event != "animatableProperty" {
+            return super.actionForKey(event)
+        }
+
+        let animation = CABasicAnimation(keyPath: event)
+        animation.timingFunction = timingFunction
+        animation.fromValue = from
+        animation.toValue = to
+        animation.duration = tweenDuration
+        animation.beginTime = CACurrentMediaTime() + delay
+        animation.delegate = self
+
+        return animation;
+    }
+
+    override func animationDidStop(anim: CAAnimation, finished flag: Bool) {
+        animationDelegate?.tweenLayerDidStopAnimation(self)
+    }
+
+    override func display() {
+        if let value = presentationLayer()?.animatableProperty {
+            animationDelegate?.tweenLayer(self, didSetAnimatableProperty: value)
+        }
+    }
+
+    func startAnimation() {
+        animatableProperty = to
     }
 }
 
